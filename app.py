09cb14db46942c54ac20c521a47231df26818807
eugeneikonya <eugeneikonya@gmail.com>
2024-08-06 20:29:52 +0300

    app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1> Eugene Did this!'
+    return '<h1> Eugene Did this! </h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
