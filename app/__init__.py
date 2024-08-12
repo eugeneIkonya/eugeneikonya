bc720bea9340a89e63b9f870582627819e630a52
eugeneikonya <eugeneikonya@gmail.com>
2024-08-12 20:34:49 +0300

    .

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 app = Flask(__name__)
@@ -11,6 +12,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key_here'
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'eugeneikonyawebsite@gmail.com'
+app.config['MAIL_PASSWORD'] = 'doykbnwmlptidfyn'
+app.config['MAIL_DEFAULT_SENDER'] = 'eugeneikonyawebsite@gmail.com'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
+
 
 db = SQLAlchemy(app)
 Migrate(app,db)
--- app/core/views.py
+++ app/core/views.py
@@ -1,7 +1,17 @@
 from flask import render_template, Blueprint
+from flask_mail import Message, Mail
+from flask_login import current_user
+from app import mail
 
 core = Blueprint('core',__name__)
 
 @core.route('/')
 def index():
     return render_template('index.html')
+
+@core.route('/sendmail')
+def send_mail():
+    msg = Message(subject='HelloMail', recipients=['eugeneikonya@gmail.com'])
+    msg.body = 'Hello Flask message sent from Flask-Mail'
+    mail.send(msg)
+    return 'Mail sent!'
